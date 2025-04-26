8ffe24b6209f27ec69a115aee568fb9698152bae
DonBatman <serfdon@gmail.com>
2025-04-26 14:31:44 -0700

    Update

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ dofile(minetest.get_modpath("myores").."/mapgen.lua")
 dofile(minetest.get_modpath("myores").."/crafting.lua")
 dofile(minetest.get_modpath("myores").."/tools.lua")
 dofile(minetest.get_modpath("myores").."/chests.lua")
+dofile(minetest.get_modpath("myores").."/stairs.lua")
 
 if minetest.get_modpath("3d_armor") then
 	dofile(minetest.get_modpath("myores").."/armor.lua")
